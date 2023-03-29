bc890d25970905ed1569d5d980c9afd0cb1b1a05
Anastasia Baburina <nastia_babur@mail.ru>
2023-03-30 01:48:37 +0300

    Выполнена задача №2

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!"); 
+﻿// Домашнее задание №1
+
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+Console.WriteLine("min = " + num2);
+Console.WriteLine("max = " + num1);
+}
+else if (num1 < num2)
+{
+Console.WriteLine("min = " + num1);
+Console.WriteLine("max = " + num2);
+}
+else
+Console.WriteLine("Числа равны");
+
