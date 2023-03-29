de2bc49151b6a8d659a438ebe9e5a7177c2780f6
Anastasia Baburina <nastia_babur@mail.ru>
2023-03-30 01:58:37 +0300

    Выполнена задача №4

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -2,7 +2,7 @@
 
 //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
 // какое число большее, а какое меньшее.
-
+/*
 Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
@@ -20,4 +20,18 @@ Console.WriteLine("max = " + num2);
 }
 else
 Console.WriteLine("Числа равны");
+*/
 
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine("max = " + max);
