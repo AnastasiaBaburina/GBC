2222a6e32e599ca2c893a596b5dc7878800e4446
Anastasia Baburina <nastia_babur@mail.ru>
2023-03-30 02:06:15 +0300

    Выполнена задача №6

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -23,7 +23,7 @@ Console.WriteLine("Числа равны");
 */
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
+/*
 Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
@@ -35,3 +35,15 @@ int max = num1;
 if (num2 > max) max = num2;
 if (num3 > max) max = num3;
 Console.WriteLine("max = " + max);
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if 
+    ((num1 % 2) == 1) Console.WriteLine("Число не является четным");
+else 
+    Console.WriteLine("Число является четным");
