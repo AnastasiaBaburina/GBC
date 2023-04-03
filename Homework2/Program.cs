40cc3c89192df6d72443595f7db891f0a09dc72c
Anastasia Baburina <nastia_babur@mail.ru>
2023-04-04 02:07:06 +0300

    Выполнена задача №15

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -20,6 +20,12 @@ int thirdDigit(int num)
     }
 }
 
+bool Weekend(int num)
+{
+    if (num == 6 || num == 7) return true;
+    else return false;
+}
+
 //Задача 10: Напишите программу, которая принимает на вход трёхзначное число
 // и на выходе показывает вторую цифру этого числа.
 /*
@@ -30,11 +36,20 @@ Console.WriteLine("Вторая цифра этого числа: " + secondDigi
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа
 // или сообщает, что третьей цифры нет.
-
+/*
 Console.Write("Введите число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 if (thirdDigit(num1) == -1)
     Console.WriteLine("Третьей цифры нет");
 else
     Console.WriteLine("Третья цифра этого числа: " + thirdDigit(num1));
+*/
 
+//Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (Weekend(num1) == true) Console.WriteLine("Да");
+if (Weekend(num1) == false) Console.WriteLine("Нет");
