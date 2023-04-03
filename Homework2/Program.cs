489ecfa6eaec1345e085f1ee1bec8dd97e361dac
Anastasia Baburina <nastia_babur@mail.ru>
2023-04-04 01:33:39 +0300

    Выполнена задача №10

---
+++ Homework2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашнее задание №2
+
+int secondDigit(int num)
+{
+    int secDigit = (num / 10) % 10;
+    return secDigit;
+}
+
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вторая цифра этого числа: " + secondDigit(num1));
+
+
