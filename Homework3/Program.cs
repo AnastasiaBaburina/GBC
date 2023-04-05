58691171cd744ce3489e465e6a4dd44af77f5ab6
Anastasia Baburina <nastia_babur@mail.ru>
2023-04-05 15:02:18 +0300

    Выполнена задача №19

---
+++ Homework3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Домашнее задание №3
+
+//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+bool palindrome (int num)
+{
+    bool flag = true;
+    if (num % 10 != num / 10000) flag = false;
+    if ((num / 10) % 10 != (num / 1000) % 10) flag = false;
+    return flag;
+}
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num >= 10000) && (num < 100000))
+{
+    if (palindrome(num) == true) Console.Write("Число является палиндромом");
+    else Console.Write("Число не является палиндромом");
+}
+else Console.Write("Вы ввели неверное число");
