36e4e8da04c4a22a5416df2d1d660d4417d6ffc9
Anastasia Baburina <nastia_babur@mail.ru>
2023-04-09 02:28:19 +0300

    Выполнена задача №29

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -21,6 +21,20 @@ int digitSum (int num)
     return sum;
 }
 
+int[] createArray (int size)
+{
+    int[] array = new int [size];
+    Console.WriteLine("Введите 8 значений: ");
+    for (int i = 0; i < size; i++) array[i] = Convert.ToInt32(Console.ReadLine());
+    return array;
+}
+
+void showArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) Console.Write(array[i] + " ");
+    Console. WriteLine();
+}
+
 //Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
 //и возводит число A в натуральную степень B.
 /*
@@ -32,7 +46,12 @@ Console.WriteLine($"{num} в степени {pow} = {numPower(num, pow)}");
 */
 //Задача 27: Напишите программу, которая принимает на вход число
 //и выдаёт сумму цифр в числе.
-
+/*
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.Write("Сумма цифр в числе: " + digitSum(num));
+*/
+//Задача 29: Напишите программу, которая задаёт массив 
+//из 8 элементов и выводит их на экран.
+
+showArray (createArray(8));
