ef6be570d07eacb25683e4d12093f84f0d4774ab
Anastasia Baburina <nastia_babur@mail.ru>
2023-04-09 01:57:20 +0300

    Выполнена задача №27

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -8,11 +8,31 @@ double numPower (double num, int pow)
     return res;
 }
 
+int digitSum (int num)
+{
+    int sum = 0;
+    do
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+    while (num > 0);
+
+    return sum;
+}
+
 //Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
 //и возводит число A в натуральную степень B.
-
+/*
 Console.Write("Введите число: ");
 double num = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите степень, в которую необходимо возвести число: ");
 int pow = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"{num} в степени {pow} = {numPower(num, pow)}");
+*/
+//Задача 27: Напишите программу, которая принимает на вход число
+//и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Сумма цифр в числе: " + digitSum(num));
