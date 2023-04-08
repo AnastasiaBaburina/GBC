3d4fab657aa5c61a470e389837281d3772f6f23b
Anastasia Baburina <nastia_babur@mail.ru>
2023-04-09 01:34:13 +0300

    Выполнена задача №4

---
+++ Homework4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Домашнее задание №4
+
+double numPower (double num, int pow)
+{
+    if (pow == 0) return 1;
+    double res =1;
+    for (int i = 1; i < pow + 1; i++) res = res * num;
+    return res;
+}
+
+//Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//и возводит число A в натуральную степень B.
+
+Console.Write("Введите число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите степень, в которую необходимо возвести число: ");
+int pow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num} в степени {pow} = {numPower(num, pow)}");
