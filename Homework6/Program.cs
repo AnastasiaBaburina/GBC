98b231e40c768e04d4ce349aaef757692c40fcb7
Anastasia Baburina <nastia_babur@mail.ru>
2023-04-13 17:31:00 +0300

    Выполнена задача №43

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -7,6 +7,18 @@ int[] CreateArray (int size)
     return array;
 }
 
+void ShowArray (int[] array)
+{
+    for (int i = 0; i<array.Length; i++) Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+void ShowDoubleArray (double[] array)
+{
+    for (int i = 0; i<array.Length; i++) Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
 int CountPositiveNumbers (int[] array)
 {
     int count = 0;
@@ -14,12 +26,36 @@ int CountPositiveNumbers (int[] array)
     return count;
 }
 
+double[] Intersection(double k1, double b1, double k2, double b2)
+{
+    double[] res = new double [2];
+    res[0] = (b2 -b1) / (k1 - k2);
+    res[1] = (res[0] * k1) + b1;
+    return res; 
+}
+
 //Задача 41: Пользователь вводит с клавиатуры M чисел.
 //Посчитайте, сколько чисел больше 0 ввёл пользователь.
-
+/*
 Console.Write("Введите значение M: ");
 int M = Convert.ToInt32(Console.ReadLine());
 
 int[] array = CreateArray(M);
 Console.WriteLine("Количество положительных чисел: " + CountPositiveNumbers(array));
+*/
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
+Console.WriteLine("Задайте значения коэффициентов");
+Console.Write("k1 = ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b1 = ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2 = ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2 = ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2) Console.WriteLine("Прямые параллельны");
+else Console.Write("Точка пересечения: ");
+ShowDoubleArray(Intersection(k1, b1, k2, b2));
