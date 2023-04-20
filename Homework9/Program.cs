51325b6c0675100d872d2542810fc54b3b00ca44
Anastasia Baburina <nastia_babur@mail.ru>
2023-04-20 23:42:05 +0300

    Выполнена задача №66

--- Homework9/Program.cs
+++ Homework9/Program.cs
@@ -14,9 +14,29 @@ void PrintNum (int N)
     }
 }
 
+int SumMtoN (int M, int N)
+{
+    if (M < N)
+    {
+        if (M != N) return M + SumMtoN(M + 1, N);
+    }
+    if (M > N)
+    {
+        if (M != N) return M + SumMtoN(M - 1, N);
+    }
+    return N;
+}
 //Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
 //Выполнить с помощью рекурсии.
-
+/*
 Console.Write("Введите N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 PrintNum(N);
+*/
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumMtoN(M,N));
+
